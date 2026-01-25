aa7c3345043e14f0e273540a604f93be3c5798f8
AllenE <evangelista.davidallen2003@gmail.com>
2026-01-25 22:06:29 +0800

    Initial Laravel 12 project setup with Vue 3, Inertia.js, and Vite configured

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="p-10">
+        <h1 class="text-3xl font-bold">
+            Barangay Issue Reporting System 🚧
+        </h1>
+        <p class="mt-4 text-gray-600">
+            Inertia + Vue is working!
+        </p>
+    </div>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Barangay Issue Reporting System</title>
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+
+<body class="antialiased">
+    @inertia
+</body>
+
+</html>
